55c080c86d51a35e43c3acc4015002dab034d068
Urjit Singh Bhatia <urjitsb87@gmail.com>
2015-09-30 11:45:36 -0700

    make connection implement closeable

--- main/main.go
+++ main/main.go
@@ -12,13 +12,14 @@ func main() {
     // The channel that takes in rabbits (rabbit jobs) and delivers them to beans (beanstalkd)
 	jobs := make(chan amqp.Delivery)
 	var waitGroup sync.WaitGroup
-
+    
 	waitGroup.Add(1)
 	go func() {
-		defer waitGroup.Done()
 		config := rabbit.Config{}
-		config.QName = "scheduler"
-		rabbit.InitAndListenQueue(config, jobs)
+		conn := rabbit.Dial(config)
+		queueName := "scheduler"
+		conn.Consume(queueName, jobs)
+		defer waitGroup.Done()
 	}()
 
 	waitGroup.Add(1)
--- rabbit/consumer.go
+++ rabbit/consumer.go
@@ -16,33 +16,30 @@ const (
 // AmqpUrl holds the amql url string and
 // AmqpConfig holds the advanced AmqpConfig like Heartbeat etc
 type Config struct {
-	QName      string      // which queue should this connect to
 	AmqpUrl    string      // amqp host url
 	AmqpConfig amqp.Config // amqp config
+}
+
+type Connection struct {
+	config         Config
+	amqpConnection *amqp.Connection
+	Close          func()
 }
 
 // InitAndListenQueue connects to the rabbitMQ queue defined in the config
 // (if it does not exit, it will error). Then it listens to messages on that
 // queue and redirects then to the jobs channnel
-func InitAndListenQueue(config Config, jobs chan<- amqp.Delivery) {
+func (conn *Connection) Produce(queueName string, jobs chan<- amqp.Delivery) {
 
-	if config.AmqpUrl == "" {
-		config.AmqpUrl = LocalhostAmqpUrl
-	}
-
-	conn, err := amqp.DialConfig(config.AmqpUrl, config.AmqpConfig)
-	rabbitbeans.FailOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close() // Clean up by closing connection when function exits
-
-	ch, err := conn.Channel()
+	ch, err := conn.amqpConnection.Channel()
 	rabbitbeans.FailOnError(err, "Failed to open a channel")
 	defer ch.Close() // Clean up by closing channel when function exits
 
 	q, err := ch.QueueInspect( // Make sure queue exists - don't create one otherwise and err.
-		config.QName, // queue name
+		queueName, // queue name
 	)
 
-	rabbitbeans.FailOnError(err, fmt.Sprintf("Failed to find queue named: %s", config.QName))
+	rabbitbeans.FailOnError(err, fmt.Sprintf("Failed to find queue named: %s", queueName))
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -59,5 +56,60 @@ func InitAndListenQueue(config Config, jobs chan<- amqp.Delivery) {
 	for d := range msgs {
 		log.Printf("Received a message: %s", d.Body)
 		jobs <- d
+	}
+}
+
+// InitAndListenQueue connects to the rabbitMQ queue defined in the config
+// (if it does not exit, it will error). Then it listens to messages on that
+// queue and redirects then to the jobs channnel
+func (conn *Connection) Consume(queueName string, jobs chan<- amqp.Delivery) {
+
+	ch, err := conn.amqpConnection.Channel()
+	rabbitbeans.FailOnError(err, "Failed to open a channel")
+	defer ch.Close() // Clean up by closing channel when function exits
+
+	q, err := ch.QueueInspect( // Make sure queue exists - don't create one otherwise and err.
+		queueName, // queue name
+	)
+
+	rabbitbeans.FailOnError(err, fmt.Sprintf("Failed to find queue named: %s", queueName))
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	rabbitbeans.FailOnError(err, "Failed to register a consumer")
+
+	log.Printf(" [*] Waiting for rabbits. To exit press CTRL+C")
+	for d := range msgs {
+		log.Printf("Received a message: %s", d.Body)
+		jobs <- d
+	}
+}
+
+// Close implements the io.Closer interface
+func (conn *Connection) Close() error {
+    
+}
+
+func Dial(config Config) *Connection {
+
+	if config.AmqpUrl == "" {
+		config.AmqpUrl = LocalhostAmqpUrl
+	}
+
+	conn, err := amqp.DialConfig(config.AmqpUrl, config.AmqpConfig)
+	rabbitbeans.FailOnError(err, "Failed to connect to RabbitMQ")
+	return &Connection{
+		config,
+		conn,
+		func() {
+			conn.Close() // Clean up by closing connection when function exits
+		},
 	}
 }
