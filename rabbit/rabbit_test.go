f6bf6bc71dbe8041880b4d934b1ebb89d89b1d2a
Urjit Singh Bhatia <urjitsb87@gmail.com>
2015-10-01 14:20:46 -0700

    add testing

---
+++ rabbit/rabbit_test.go
@@ -0,0 +1,12 @@
+package rabbit_test
+
+import (
+	. "github.com/urjitbhatia/rabbitbeans/rabbit"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Rabbit", func() {
+
+})
